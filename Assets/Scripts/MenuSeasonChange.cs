22d3ee87f3b50b9c28acace8e8e59fa9d7c26f7d
StardustDragon1 <chrisfodor666@gmail.com>
2020-03-30 23:15:23 +0200

    GX background change

---
+++ Assets/Scripts/MenuSeasonChange.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSeasonChange : MonoBehaviour
+{
+
+    public Image logo_main;
+    public Image logo_play;
+
+    public Image main_bg;
+    public Image play_bg;
+    public Image options_bg;
+    public Image collection_bg;
+    public Image full_bg;
+
+
+    public Sprite gxbg;
+    public Sprite dmbg;
+    public Sprite DM_logo;
+    public Sprite GX_logo;
+
+
+
+
+
+    void Start()
+    {
+ 
+
+       
+            if (PlayerPrefs.GetString("Series") == "GX")
+                {
+            logo_main.sprite = GX_logo;
+            logo_main.rectTransform.sizeDelta = new Vector2(300, 140);
+            logo_play.sprite = GX_logo;
+            logo_play.rectTransform.sizeDelta = new Vector2(300, 140);
+
+            main_bg.sprite = gxbg;
+            play_bg.sprite = gxbg;
+            options_bg.sprite = gxbg;
+            collection_bg.sprite = gxbg;
+            full_bg.sprite = gxbg;
+
+
+
+        } else {
+            logo_main.sprite = DM_logo;
+            logo_main.rectTransform.sizeDelta = new Vector2(300, 100);
+            logo_play.sprite = DM_logo;
+            logo_play.rectTransform.sizeDelta = new Vector2(300, 100);
+
+            main_bg.sprite = dmbg;
+            play_bg.sprite = dmbg;
+            options_bg.sprite = dmbg;
+            collection_bg.sprite = dmbg;
+            full_bg.sprite = dmbg;
+        }
+               
+            
+
+
+
+        
+
+
+    }
+
+
+
+
+
+
+}
--- Assets/Scripts/ThemeSongScript.cs
+++ Assets/Scripts/ThemeSongScript.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class ThemeSongScript : MonoBehaviour
 {
 
+    AudioSource audio_source;
+    public AudioClip DM_theme;
+    public AudioClip GX_theme;
+
+
+
+
     private static ThemeSongScript instance = null;
     public static ThemeSongScript Instance
     {
         get { return instance; }
     }
+
+    void Start()
+    {
+        audio_source = gameObject.GetComponent<AudioSource>();
+        if (PlayerPrefs.GetString("Series") == "GX")
+        {
+            audio_source.clip = GX_theme;
+            audio_source.Play();
+        } else
+        {
+            audio_source.clip = DM_theme;
+            audio_source.Play();
+        }
+
+    }
+
+
 
     void Awake()
     {
